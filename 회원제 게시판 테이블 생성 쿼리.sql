b5f84c2216e3b0711a196e39bd150f1332c8e9eb
neoSteelrain <magnosix6@gmail.com>
2022-11-05 00:20:56 +0900

    프로젝트 구조 재설정

--- 회원제 게시판 테이블 생성 쿼리.sql
+++ 회원제 게시판 테이블 생성 쿼리.sql
@@ -1,31 +1,36 @@
-CREATE TABLE `member_table` (
-   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-   `memberEmail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL unique,
-   `memberPassword` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-   `memberName` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-   `memberMobile` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-   `memberProfile` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-   PRIMARY KEY (`id`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+create database db_springframework;
+use db_springframework;
+create user 'user_springframework'@'localhost' identified by '1234';
+grant all privileges on db_springframework.* to user_springframework@localhost;
 
-CREATE TABLE `board_table` (
-   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-   `boardTitle` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-   `boardWriter` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-   `boardContents` varchar(4000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-   `boardHits` int unsigned DEFAULT '0',
-   `boardCreatedDate` datetime DEFAULT CURRENT_TIMESTAMP,
-   `boardFileName` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-   PRIMARY KEY (`id`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
- 
-CREATE TABLE comment_table (
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  boardId BIGINT UNSIGNED not null,
-  commentWriter VARCHAR(50) not null,
-  commentCreatedDate DATETIME DEFAULT now(),
-  PRIMARY KEY (id),
-  constraint fk_comment foreign key(boardId) references board_table(id) on delete cascade)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE `member_table` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `memberEmail` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
+    `memberPassword` VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `memberName` VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `memberMobile` VARCHAR(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `memberProfile` VARCHAR(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+    CREATE TABLE `board_table` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `boardTitle` VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `boardWriter` VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `boardContents` VARCHAR(2000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `boardHits` INT UNSIGNED DEFAULT '0',
+    `boardCreatedDate` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `boardFileName` VARCHAR(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `memberId` BIGINT UNSIGNED NOT NULL,
+    constraint fk_board_table_memberId foreign key(memberId) references member_table(id) on delete cascade
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `comment_table` (
+     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `boardId` BIGINT UNSIGNED NOT NULL,
+     `memberId` BIGINT UNSIGNED NOT NULL,
+     `commentWriter` VARCHAR(50) NOT NULL,
+     `comment_contents` VARCHAR(500),
+     `commentCreatedDate` DATETIME DEFAULT CURRENT_TIMESTAMP,
+     constraint fk_comment_memberId foreign key(memberId) references member_table(id) on delete cascade,
+     constraint fk_comment_boardId foreign key(boardId) references board_table(id) on delete cascade
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
